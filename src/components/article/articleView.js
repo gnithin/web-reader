641bb6957032f3404e5898ca2f1e578603c5f5bc
Nithin <nithin.linkin@gmail.com>
2020-01-27 15:57:39 -0500

    [scroll-change] Break the section and sub-section into functional components

--- src/components/article/articleView.js
+++ src/components/article/articleView.js
@@ -19,29 +19,38 @@ class ArticleView extends Component {
                 <h1 className="article-title">{article.title}</h1>
                 <hr />
                 <div className="section-container">
-                    {article.sections.map((section) => {
-                        return (
-                            <div key={section.number} className="section" id={`${section.number}`}>
-                                <h2>{section.number} {section.title}</h2>
-                                <div className="sub-section-container">
-                                    {section.subSections.map((ss) => {
-                                        return (
-                                            <div key={ss.number} className="sub-section" id={`${section.number}-${ss.number}`}>
-                                                <h3> {ss.number} {ss.title} </h3>
-                                                <div className="sub-section-content">
-                                                    {ss.content}
-                                                </div>
-                                            </div>
-                                        );
-                                    })}
-                                </div>
-                            </div>
-                        );
-                    })}
+                    {article.sections.map((section) => (
+                        <SectionComponent key={section.number} section={section} />
+                    ))}
                 </div>
             </React.Fragment>
         )
     }
+}
+
+const SectionComponent = ({ section }) => {
+    console.log(section)
+    return (
+        <div className="section" id={`${section.number}`}>
+            <h2>{section.number} {section.title}</h2>
+            <div className="sub-section-container">
+                {section.subSections.map((ss) => (
+                    <SubSectionComponent key={ss.number} ss={ss} sectionNumber={section.number} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+const SubSectionComponent = ({ ss, sectionNumber }) => {
+    return (
+        <div className="sub-section" id={`${sectionNumber}-${ss.number}`}>
+            <h3> {ss.number} {ss.title} </h3>
+            <div className="sub-section-content">
+                {ss.content}
+            </div>
+        </div>
+    );
 }
 
 ArticleView.propTypes = {
