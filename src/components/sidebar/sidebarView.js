b4fafaee0fb58202d01d5eb97136d98e1f312aaf
Nithin <nithin.linkin@gmail.com>
2020-01-27 14:33:48 -0500

    [side-bar] Add the basic view to the side-bar

--- src/components/sidebar/sidebarView.js
+++ src/components/sidebar/sidebarView.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Article from 'models/article'
+import './sidebar.css'
 
 class SidebarView extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-
+            article: props.data
         }
     }
 
     render() {
+        let article = this.state.article;
         return (
-            <div>Sidebar</div>
+            <React.Fragment>
+                <div className="sidebar-title">
+                    {article.title}
+                </div>
+                <ul>
+                    {article.sections.map((section) => {
+                        return (
+                            <li key={section.number} className="section-list">
+                                <div className="section-title">{section.number}. {section.title}</div>
+                                <ul className="list-group">
+                                    {section.subSections.map((ss) => {
+                                        return (
+                                            <li key={ss.number}>
+                                                <span className="sub-section-title">{ss.number}. {ss.title}</span>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </React.Fragment>
         )
     }
+}
+
+SidebarView.propTypes = {
+    data: PropTypes.instanceOf(Article)
 }
 
 export default SidebarView
