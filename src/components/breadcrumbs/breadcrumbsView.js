1dc72373be69b6ff182ef1b328325a10df624b1b
Nithin <nithin.linkin@gmail.com>
2020-01-28 00:29:19 -0500

    [section-change] Add bread-crumbs whenever the section changes

--- src/components/breadcrumbs/breadcrumbsView.js
+++ src/components/breadcrumbs/breadcrumbsView.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Article from 'models/article'
+import Utils from 'common/utils'
+import './breadcrumbs.css'
 
 class BreadcrumbsView extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {
+        let visibleSection = props.visibleSection;
+        if (Utils.isNull(visibleSection)) {
+            visibleSection = 1
+        }
 
+        this.state = {
+            visibleSection: visibleSection,
+            data: props.data,
+        }
+
+        this.crumbsMap = this.computeCrumbs(props.data);
+    }
+
+    computeCrumbs(data) {
+        let cMap = {}
+        if (Utils.isNull(data)) {
+            return cMap;
+        }
+
+        let title = data.title;
+        for (let section of data.sections) {
+            cMap[section.number] = [title, `${section.number}. ${section.title}`]
+            for (let ss of section.subSections) {
+                cMap[ss.number] = [title, `${section.number}. ${section.title}`, `${ss.number}. ${ss.title}`]
+            }
+        }
+        return cMap;
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (newProps.visibleSection !== this.props.visibleSection) {
+            this.setState({ visibleSection: newProps.visibleSection });
         }
     }
 
     render() {
+        let crumb = this.crumbsMap[this.state.visibleSection]
+        if (Utils.isNull(crumb)) {
+            crumb = [this.state.data.title]
+        }
+
         return (
-            <div>BreadcrumbsView</div>
+            <div className="crumb-container">
+                {crumb.map((c) => {
+                    return (<div>{c}</div>);
+                })}
+            </div>
         )
     }
+}
+
+BreadcrumbsView.propTypes = {
+    data: PropTypes.instanceOf(Article).isRequired,
+    visibleSection: PropTypes.number.isRequired,
 }
 
 export default BreadcrumbsView
