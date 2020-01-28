5e62fc95352a6f5c93fe66b00743f7ee32a588bd
Nithin <nithin.linkin@gmail.com>
2020-01-28 01:20:21 -0500

    [text-break] Add more content

--- src/constants/article.js
+++ src/constants/article.js
@@ -6,14 +6,17 @@ module.exports = {
             number: 1,
             sub_sections: [
                 {
-                    title: "Summary-1",
+                    title: "Summary",
                     number: 1.1,
                     content: "Northeastern University (NU or NEU) is a private research university in Boston, Massachusetts, established in 1898. It is categorized as an R1 institution (Doctoral Universities: Highest Research Activity) by the Carnegie Classification of Institutions of Higher Education.[4] The university offers undergraduate and graduate programs on its main campus in Boston. The university has satellite campuses in Charlotte, North Carolina; Seattle, Washington; San Jose, California; and Toronto, Canada, that exclusively offer graduate degrees. Northeastern recently purchased the New College of the Humanities in London and plans to open an additional campus in Vancouver, Canada. The university's enrollment is approximately 18,000 undergraduate students and 8,000 graduate students"
                 },
                 {
-                    title: "Summary-2",
+                    title: "University",
                     number: 1.2,
-                    content: "Northeastern University (NU or NEU) is a private research university in Boston, Massachusetts, established in 1898. It is categorized as an R1 institution (Doctoral Universities: Highest Research Activity) by the Carnegie Classification of Institutions of Higher Education.[4] The university offers undergraduate and graduate programs on its main campus in Boston. The university has satellite campuses in Charlotte, North Carolina; Seattle, Washington; San Jose, California; and Toronto, Canada, that exclusively offer graduate degrees. Northeastern recently purchased the New College of the Humanities in London and plans to open an additional campus in Vancouver, Canada. The university's enrollment is approximately 18,000 undergraduate students and 8,000 graduate students"
+                    content: `
+                    Northeastern features a cooperative education program, more commonly known as "co-op", that integrates classroom study with professional experience and contains over 3,100 partners across all seven continents.[6] The program has been a key part of Northeastern's curriculum of experiential learning for more than a hundred years and is one of the largest co-op/internship programs in the world. While it is not required for students of all academic disciplines to participate in the co-op program, participation is nearly universal among undergraduate students as it helps distinguish their university experience from that of other universities. Northeastern also has a comprehensive study abroad program that spans more than 170 universities and colleges.[7]
+                    Northeastern is a large, highly residential university. Most students choose to live on campus but upperclassmen have the option to live off campus. More than 75% of Northeastern students receive some form of financial aid. In the 2019–20 school year, the university has committed $296.2 million in grant and scholarship assistance.[8] 
+                    `,
                 }
             ]
         },
@@ -34,6 +37,59 @@ module.exports = {
                     content: `ortheastern's men's and women's hockey teams compete in the Hockey East Conference. During the 2007–2008 season, the men's team ranked as high at #7 in the country and held the top spot in the conference before finishing the season in sixth place in Hockey East. Both teams also participate in the annual Beanpot tournament between the four major Boston-area colleges. Northeastern's men's team has won the annual event 4 times in its 54-year history, while the women's team has captured the Beanpot 14 times. During the 2008–2009 season, the men's team ranked as high as 3rd in the nation and held the top spot in Hockey East until the last weekend of the season; the team made the NCAA tournament for the first time since 1994, the Beanpot championship game for the first time since 2004, and goalie Brad Thiessen made the Hobey Hat Trick, only the second Northeastern player to do so. Northeastern won the 2018 Beanpot championship by defeating Boston College 3-0 in the first match and defeating Boston University 5-2 in the final match. The victory came after Northeastern attained the highest placement in the 2017-2018 standings of the Beanpot competitors. The Beanpot also presents two awards to individual players. One is for the most valuable player and one is to the best goalie (determined by best save percentage). The second award is named the Eberly award after Glen and Dan Eberly who were goalies at Northeastern and Boston University. In addition to winning the Beanpot title, Northeastern took home both awards with the award for most valuable player being presented to Adam Gaudette and the Eberly Award being presented to Cayden Primeau who had a save percentage of .974 (making him the goalie with second highest save percentage to win the award in the 44 years the award has been given).[113]`
                 }
             ]
-        }
+        },
+        {
+            title: "Campus",
+            number: 3,
+            sub_sections: [
+                {
+                    title: "Boston",
+                    number: 3.1,
+                    content: `
+                    Northeastern is located in Boston's Fenway, Roxbury, South End, and Back Bay neighborhoods adjacent to Huntington Avenue near the Museum of Fine Arts and Symphony Hall. The area is also known as the Fenway Cultural District.[120]
+
+Although located in the heart of Boston, the Northeastern campus is still filled with trees, flowers, and grassy quads. Since the late 1990s, Northeastern has been considered a model of design for urban universities and has twice won the "most beautiful new or renovated exterior space" award (presented by the American Institute of Architects) in 2001 and 2004.[121] In 2019, the campus was officially designated as an arboretum by ArbNet, making it the only campus in Boston to receive the designation.[122][123]
+
+The first baseball World Series took place on the Huntington Avenue Grounds, now part of the Northeastern University campus. The site is commemorated in front of Churchill Hall by a statue of Cy Young.[124]
+                    `
+                },
+                {
+                    title: "Sustainability",
+                    number: 3.2,
+                    content: `
+                    The 2011 Sustainable Endowments Institute's College Sustainability Report Card issued Northeastern a grade of "A-" for its environmental sustainability efforts and programs.[125] Additionally, the Princeton Review rated Northeastern as one of the top 15 "Green Colleges" in the nation in 2010.[126] In 2011, the GreenMetric World University ranking evaluated Northeastern as the second greenest university in the world, and first in the US.[127] Northeastern placed first in the rankings again in 2014.[128]
+
+In accordance with a Boston zoning code amendment in 2007,[129] International Village residence hall was certified as a LEED Gold building in 2010.[130] Dockser Hall was the first building on campus to achieve LEED certification, also Gold, with the completion of its renovation in 2010.[131] East Village was rated LEED Silver in 2016 and the Interdisciplinary Science and Engineering Complex was rated LEED Gold in 2018.[132][133]
+                    `
+                }
+            ]
+        },
+        {
+            title: "Academics",
+            number: 4,
+            sub_sections: [
+                {
+                    title: "Coop",
+                    number: 4.1,
+                    content: `
+                    Northeastern offers undergraduate majors in 65 departments. At the graduate level, there are about 125 programs. A Northeastern education is interdisciplinary and entrepreneurial. Founded in 2009, IDEA is Northeastern University's student-led Venture Accelerator, which provides entrepreneurs, including students, faculty, and alumni in the Northeastern community with the necessary support and educational experience towards developing a business from core concept to launch. Academics at Northeastern is grounded in a liberal arts education and the integration of classroom studies with experiential learning opportunities, including cooperative education, student research, service learning, and global experience, including study abroad and international co-op. The university's cooperative education program places about 5,000 students annually with more than 3,000 co-op employers in Boston, across the United States, and around the globe. In 2014, College Prowler gave Northeastern an "A+" rating for the quality of classes, professors, and overall academic environment.
+
+                    The University Honors Program offers selected students an enhanced curriculum. These students are selected from the regular applicant pool with no separate application and represent the applicants with the highest GPA and SAT/ACT scores that year. The culminating experience is advanced specialty work in a major field through college-specific choices including specialized advanced honors seminars and an independent research project.[66] In addition, students in the Honors Program exclusively can live in a Living-Learning Community housed in West Villages C[67] and F. In Fall 2009, the university began housing first-year Honors students in the lower nine floors of the newly constructed International Village residence hall. Starting in Fall 2017, these students are housed in the lower floors of the even newer 17 story East Village residence hall. 2017 also marked the beginning of the Honors Discovery course and the introduction of the Student Assessed Integrated Learning (SAIL) app.[68]
+
+                    Northeastern has one of the largest co-op/internship programs in the world.[69] Started in 1909, NU's co-op program is one of the oldest in the nation. In the co-op program, students alternate periods of academic study with periods of professional employment (usually paid) related to their major. Most majors offer a four-year graduation option with fewer co-op placements, but the five-year program is slightly more popular with students. Students on co-op do not pay tuition and students not living on campus do not pay room and board. The co-op program typically begins the spring of the second year or fall of the third year (after a more traditional program for the first semesters on campus). Students usually take anywhere between one and three with 96% participating in one and 78% participating in two or more.[70]
+
+Co-op placements range from small start-up companies to large multinational companies, including many Fortune 500 corporations. The program also places students with government agencies, branches of government, nonprofits, and non-governmental organizations. Northeastern students can be found interning in the United States Congress, the White House, United Nations, and at NASA. Student placements usually last six months and most of the time, students are paid. Students may live in the university residence halls on campus during co-op employment, and the university currently leases housing for students co-oping in New York City, San Francisco, Seattle and Washington, D.C. and assists elsewhere.[71]
+
+Northeastern co-op students staying in Boston usually benefit from the fact that the city's most prominent industries have numerous offices/headquarters there. Boston's most consequential industries such as its financial sector, technology sector, and medicine/life sciences sector tend to hire many co-ops in these fields especially to big-name companies. When it comes to D'amore-Mckim Business students, many co-ops end up working for large financial firms such as State Street and John Hancock Financial. Accounting firms such as PricewaterhouseCoopers (PwC) and Deloitte & Touche as well as consulting firms such as Boston Consulting Group tend to hire many co-ops every cycle as well. Retail companies like TJX Companies and Staples tend to hire several marketing students every cycle.[72]
+
+Some students also decide to develop their own co-ops if they wish to do something that is not offered. This usually involves starting a new company or reaching out to a company Northeastern does not have a partnership with and facilitating the creation of a co-op partnership. Many companies continually pass down their co-op opportunities to Northeastern students so these new partnerships are documented in Northeastern's co-op database, known as NUCareers, to be given to future Northeastern students. Students that decide to start their own companies are usually involved with IDEA, Northeastern's Venture Accelerator, and will sometimes spend their entire two or three co-ops solely developing their companies. They might also join the Husky Startup Challenge which also helps develop student-run companies. If a company is successful in either IDEA, the Husky Startup Challenge or both, they often to move onto MassChallenge in Boston which is a huge global non-profit startup accelerator and competition.[73]
+
+All Northeastern students take at least one class which prepares them for their co-op and the expectations of a given industry. Some schools like D'amore-Mckim have students take three different one-credit classes to prepare them for their co-op. During these classes, students work with advisers inside and outside of class to pursue potential co-ops as well as work on strategies to make themselves more competitive against other candidates.[74]
+
+The co-op program has led to the university's high reputation when it comes to job placement. 50% of Northeastern students receive a job offer from a previous co-op employer as of 2017.[6] 92% were either employed or enrolled in graduate school 9 months after graduation.[75] This has also led Northeastern to consistently rank within the top 5 in the Princeton Review's list for "Best Career Services/Internships" within the last decade, mainly taking the top spot. The list split into "Best Career Services" and "Best Internships" in 2016 and Northeastern currently ranks 3rd for career services and 5th for internships in the United States.[76][77] 
+                    `
+                },
+            ]
+        },
     ]
 }
