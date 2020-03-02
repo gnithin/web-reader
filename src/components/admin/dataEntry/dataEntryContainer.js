883c52f31ca0bffb2b2ed8bd635f191bd87b3b5b
Nithin <nithin.linkin@gmail.com>
2020-03-02 14:34:35 -0500

    [admin] Add the data-entry-view

--- src/components/admin/dataEntry/dataEntryContainer.js
+++ src/components/admin/dataEntry/dataEntryContainer.js
@@ -1,11 +1,10 @@
 import React, {Component} from 'react';
+import DataEntryView from "./dataEntryView";
 
 class DataEntryContainer extends Component {
     render() {
         return (
-            <div>
-                Data entry component
-            </div>
+            <DataEntryView/>
         );
     }
 }
