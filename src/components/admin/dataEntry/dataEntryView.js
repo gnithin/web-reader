9a1c275abafff961ea52948c1475076f2e1b5d5c
Nithin <nithin.linkin@gmail.com>
2020-03-02 15:15:49 -0500

    [data-entry] Add the basic structure of the data-entry page

--- src/components/admin/dataEntry/dataEntryView.js
+++ src/components/admin/dataEntry/dataEntryView.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import './dataEntry.css'
 
 const DataEntryView = () => {
     return (
-        <div>
-            Data entry component
+        <div className="container-fluid data-entry-container">
+            <div className="row da-input-entry da-heading">
+                <h3>Data Entry</h3>
+            </div>
+            <div className="row da-input-entry">
+                <div className="col-12">
+                    <input
+                        type="string"
+                        id="title"
+                        placeholder="Title"
+                        className="form-control"
+                    />
+                </div>
+            </div>
+
+            <div className="row da-input-entry">
+                <div className="col-12">
+                    <textarea
+                        id="content"
+                        placeholder="Content"
+                        className="form-control"
+                    />
+                </div>
+            </div>
+
+            <div className="row da-input-entry">
+                <div className="col-12">
+                    <select className="custom-select">
+                        <option>Dummy</option>
+                        <option>Dummy</option>
+                        <option>Dummy</option>
+                        <option>Dummy</option>
+                        <option>Dummy</option>
+                    </select>
+                </div>
+            </div>
+
+            <div className="row da-input-entry">
+                <div className="col-12">
+                    <div className="custom-file">
+                        <input type="file" className="custom-file-input" id="customFile"/>
+                        <label className="custom-file-label" htmlFor="customFile">Choose
+                            file
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row da-input-entry">
+                <div className="col-12">
+                    <button className="btn btn-primary">Add Entry</button>
+                </div>
+            </div>
+
         </div>
     );
 };
