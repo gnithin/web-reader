eff80f9ef7dbd3fa5cac40319cf82322621631fb
Nithin <nithin.linkin@gmail.com>
2020-03-05 13:18:50 -0500

    [reader] Fix the incorrect action-name

--- src/redux/actions/articlesListActions.js
+++ src/redux/actions/articlesListActions.js
@@ -1,6 +1,6 @@
 import ActionUtils from "./actionUtils";
 
-export const ACTION_UPDATE_ARTICLES_LIST = "add-article";
+export const ACTION_UPDATE_ARTICLES_LIST = "add-articles-list";
 
 export default class ArticlesListActions {
     static updateArticlesList(articlesList) {
