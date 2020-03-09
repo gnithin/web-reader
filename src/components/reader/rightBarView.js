672e7b7ce95488b6a52c55cecd68ea340f092f48
Nithin <nithin.linkin@gmail.com>
2020-03-09 15:47:44 -0400

    [reader] Add the basic structure of the right-bar

---
+++ src/components/reader/rightBarView.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import Utils from "../../common/utils";
+import {withRouter} from "react-router-dom";
+import "./rightBar.css";
+
+class RightBarView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: "",
+        }
+    }
+
+    render() {
+        return (
+            <div className="right-bar-container row no-gutters">
+                <div className="col-10">
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={this.state.searchText}
+                        placeholder="Search for keywords here..."
+                        onChange={(e) => {
+                            this.setState({
+                                              searchText: e.target.value,
+                                          });
+                        }}
+                        onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                                this.performSearch()
+                            }
+                        }}
+                    />
+                </div>
+
+                <div className="col-2">
+                    <button className="btn btn-primary" onClick={(e) => {
+                        this.performSearch();
+                    }}>
+                        <i className="fa fa-arrow-right" aria-hidden="true"></i>
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+    performSearch() {
+        if (Utils.isEmptyStr(this.state.searchText)) {
+            console.log("Cannot perform search since the text is empty!");
+            return;
+        }
+
+        this.props.history.push(`/search?title=${this.state.searchText}`);
+    }
+}
+
+export default withRouter(RightBarView);
